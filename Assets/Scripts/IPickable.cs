bf866c6112dbd670737d8eb357aeadbca69022e2
iulia.todor.7e6 <iulia.todor.7e6@itb.cat>
2023-11-23 19:49:07 +0100

    Create Items, Inventario and NPC

---
+++ Assets/Scripts/IPickable.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPickable
+{
+    public void HandlePickUp()
+    {
+
+    }
+
+    public void HandleThrow()
+    {
+
+    }
+}
